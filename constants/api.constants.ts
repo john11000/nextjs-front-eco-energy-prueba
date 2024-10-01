960d9e8cf70008a894b33d66dc973ab307ff0ead
john11000 <86936709+john11000@users.noreply.github.com>
2024-10-01 21:58:12 +0000

    Update api.constants.ts

--- constants/api.constants.ts
+++ constants/api.constants.ts
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
-  process.env.API_BASE_URL || "http://localhost:8000/api/v1";
+  process.env.NEXT_API_BASE_URL || "http://localhost:8000/api/v1";
 export const API_STASTICS_CLIENT_URL = `${API_BASE_URL}/client-statistics`;
